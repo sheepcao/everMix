be62d054891fe4b5d06206a6b666ab01377070d5
sheepcao <sheepcao1986@163.com>
2014-12-09 14:36:27 +0800

    Initial Commit

---
+++ mixMusicTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mixMusicTest
+//
+//  Created by Eric Cao on 12/9/14.
+//  Copyright (c) 2014 Eric Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
